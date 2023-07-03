bbf079fae986f9b85ba5fead80f93fe319c1a240
rom064 <rom064@mail.ru>
2023-07-03 22:38:32 +0500

    ДЗ Алгоритмы часть-1

---
+++ src/main/java/ru/algorithm1/Algorithm1Application.java
@@ -0,0 +1,13 @@
+package ru.algorithm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithm1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithm1Application.class, args);
+    }
+
+}
